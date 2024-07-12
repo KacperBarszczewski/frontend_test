00fb89ca0eb4b97b794b4bdd61a6c91e2ce7c05d
KacperB <kacpero.barszczewski@gmail.com>
2024-07-12 17:49:24 +0200

    style main page

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { BlockFirstComponent } from '../../components/block-first/block-first.component';
+import { BlockSecondComponent } from '../../components/block-second/block-second.component';
+import { BlockThirdComponent } from '../../components/block-third/block-third.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,FooterComponent,BlockFirstComponent,BlockSecondComponent,BlockThirdComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
